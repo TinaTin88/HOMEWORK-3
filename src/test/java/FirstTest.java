4c1d3994f3790c0b9df1b1e2ebfcbaa45e5f704b
TinaTin88 <tiko.iobishvili@gmail.com>
2023-05-16 17:21:42 +0400

    practice works

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        // open("https://smartacademy.ge/");
+        //$(byId("search")).setValue("ტესტირების ავტომატიზაციის საფუძვლები").pressEnter();
+       // sleep(5000);
+
+        open ("https://ee.ge/");
+
+
+        }
+
+
+
+
+
+
+    }
+
